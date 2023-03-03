5af33296757f011adf7ccd40d697fa9597d9d233
Rain <lgy.1992.cn@gmail.com>
2023-03-04 03:54:27 +0800

    [api]配置zap logger

--- api/api.go
+++ api/api.go
@@ -1,1 +1,20 @@
 package api
+
+import (
+	"time"
+
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+var app = gin.New()
+
+func init() {
+	app.Use(ginzap.Ginzap(zap.L(), time.RFC3339, true))
+	app.Use(ginzap.RecoveryWithZap(zap.L(), true))
+}
+
+func Run() {
+	app.Run()
+}
