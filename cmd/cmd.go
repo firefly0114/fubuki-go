82b7a8e2125f33edd6f52956ca8a1aaa15263720
Rain <lgy.1992.cn@gmail.com>
2023-03-06 01:48:36 +0800

    [cmd]增加基本的参数解析

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,1 +1,14 @@
 package cmd
+
+import "flag"
+
+var (
+	Debug          bool
+	ApiListingPort int
+)
+
+func init() {
+	flag.BoolVar(&Debug, "d", false, "enable debug mode")
+	flag.IntVar(&ApiListingPort, "api-port", 9900, "manage api port")
+	flag.Parse()
+}
