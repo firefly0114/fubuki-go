d13db7ca2a1883188f9ea83e710d9316ab80897e
Rain <lgy.1992.cn@gmail.com>
2023-03-06 01:53:43 +0800

    [main]增加goroutine启动逻辑，当有一个任务失败时结束程序

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fubuki-go/api"
+	"fubuki-go/log"
+	"fubuki-go/manager"
 
+	"go.uber.org/zap"
+)
+
+func main() {
+	ctx, cancel := context.WithCancelCause(context.Background())
+	go func() {
+		cancel(api.Run())
+	}()
+
+	go func() {
+		cancel(manager.Manager.Run())
+	}()
+
+	<-ctx.Done()
+	log.Fatal("goroutine was exited", zap.Error(context.Cause(ctx)))
 }
