aa50806565e98dc32d3ba1c137894f53f65f2a58
Rain <lgy.1992.cn@gmail.com>
2023-03-06 01:50:31 +0800

    [manager] init

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,25 @@
 package manager
+
+import (
+	"errors"
+	"fmt"
+)
+
+var Manager = newManager()
+
+type manager struct {
+}
+
+func newManager() *manager {
+	return new(manager)
+}
+
+func (m *manager) Run() error {
+	// TODO: 实现manager逻辑，manager.Run 应返回错误
+	return errors.New("error")
+}
+
+func Run() error {
+	err := Manager.Run()
+	return fmt.Errorf("manager exited, %w", err)
+}
